e7099bc5fa35f8c01660a7c615d43178787489f6
Nadine Amanda <nadine@soar.earth>
2021-10-20 14:00:13 +0800

    new css for expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 10, 20);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 295;
+
+
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2> Expense Item </h2>
-        <div> Amount </div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString}</div>
+      <div className="expense-item__descirption">
+        <h2> {expenseTitle} </h2>
+        <div className="expense-item__price"> ${expenseAmount} </div>
       </div>
     </div>
   );
