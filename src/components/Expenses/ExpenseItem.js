3917b70beebe0bbb8910b2241155a1c09d8008a1
Nadine Amanda <nadine@soar.earth>
2021-10-21 12:47:34 +0800

    Added sub folders

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import "./Expenses.js";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__descirption">
+        <h2> {props.title} </h2>
+        <div className="expense-item__price"> ${props.amount} </div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
