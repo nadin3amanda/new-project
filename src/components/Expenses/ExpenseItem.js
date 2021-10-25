2e481d8ae7f9386c0f1122d10aff9b0cdee33fde
Nadine Amanda <nadine@soar.earth>
2021-10-25 16:12:49 +0800

    Added key component. Issue #3 still present

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,11 +8,10 @@ const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item__descirption">
-        <h2> {props.title} </h2>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
-    
     </Card>
   );
 };
